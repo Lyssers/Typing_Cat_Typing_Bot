9b403ae78be4fdb803f18440fa644deebbe80a3e
Lyssers <37519050+Lyssers@users.noreply.github.com>
2020-06-29 03:01:32 +0100

    added Windows support

--- TypingBot.py
+++ TypingBot.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import platform
+import re
 import time
+import platform
 from pathlib import Path, PureWindowsPath
 import os
 
@@ -17,34 +17,34 @@ else:
 extensions = [
     "https-everywhere@eff.org.xpi", #HTTPS Everywhere
     "uBlock0@raymondhill.net.xpi", #Ublock Origin
-    #"jid1-MnnxcxisBPnSXQ@jetpack.xpi" # Privacy Badger
+    #"jid1-MnnxcxisBPnSXQ@jetpack.xpi" #Privacy Badger
     #Add Your Extension here
     ]
 
-
+#Going on TypingCat
 firefox = webdriver.Firefox()
+firefox.get('https://thetypingcat.com/typing-speed-test/1m') #Change to 2m or 5m according to preference
 
 #Installing Extensions
-#Sometimes extensions disappear when navigating to a different URL, so we do it after we go to the website
-#This can cause some ads to still display
 for extension in extensions:
-    firefox.install_addon(extensionPath + extension, temporary=True)
-#Going on 10FastFingers
-firefox.get('https://10fastfingers.com/typing-test/english')
+   firefox.install_addon(extensionPath + extension, temporary=True)
 
-#Sleep so the website can load and not slow us down loading
+#Sleep so the website can load and not slow us down
 time.sleep(5) #Seconds
 
 # Using javascript to get the typing content from the website and storing value in "string" variable
-#Type the word, then type space for every word.
-while firefox.execute_script('return document.querySelectorAll(".highlight").length') != 0:
-	string = firefox.execute_script('return document.querySelectorAll(".highlight")[0].innerHTML')
-	print(string)
-	
-    #Type the text!
-	action = ActionChains(firefox)
-	action.send_keys(string)
-	action.perform()
-	action = ActionChains(firefox)
-	action.send_keys(Keys.SPACE)
-	action.perform()
+string = ''
+for i in range(firefox.execute_script('return document.querySelectorAll(".line").length')):
+	string += firefox.execute_script('return document.querySelectorAll(".line")['+str(i)+'].innerHTML')
+
+#Regex to delete tags present inside the string
+string = re.sub(r'<[^>]*>','',string) 
+
+#Regex to replace the '⏎' symbol with a carriage return so enter it presses it for us
+string = re.sub(r'\u23CE', '\\r',string) #Using \n will result in non-100% accuracy
+
+#Type the text!
+action = ActionChains(firefox)
+action.send_keys(" ") #Since we have to press a key to start the game first.
+action.send_keys(string)
+action.perform()
