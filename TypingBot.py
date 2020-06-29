72ab7bd055120c759b781ea8c3825afd5f34c7e6
Lyssers <37519050+Lyssers@users.noreply.github.com>
2020-06-29 02:58:36 +0100

    windows path support

--- TypingBot.py
+++ TypingBot.py
@@ -1,39 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-import re
+from selenium.webdriver.common.keys import Keys
+import platform
 import time
+from pathlib import Path, PureWindowsPath
+import os
 
 #Enabling Extension Support:
-#Change q634iaqw.default-esr to your Firefox profile location
+#Check OS
+#Change agv1sry4.default or q634iaqw.default-esr to your Firefox profile location name
+if platform.system() == 'Windows':
+       extensionPath = str(Path.home()) + str(PureWindowsPath('/AppData/Roaming/Mozilla/Firefox/Profiles/agv1sry4.default/extensions')) + os.sep
+else:
+    extensionPath =  "~/.mozilla/firefox/q634iaqw.default-esr/extensions/"
+      
 extensions = [
-    "~/.mozilla/firefox/q634iaqw.default-esr/extensions/https-everywhere@eff.org.xpi", #HTTPS Everywhere
-    "~/.mozilla/firefox/q634iaqw.default-esr/extensions/uBlock0@raymondhill.net.xpi", #Ublock Origin
+    "https-everywhere@eff.org.xpi", #HTTPS Everywhere
+    "uBlock0@raymondhill.net.xpi", #Ublock Origin
+    #"jid1-MnnxcxisBPnSXQ@jetpack.xpi" # Privacy Badger
+    #Add Your Extension here
     ]
 
-#Going on TypingCat
+
 firefox = webdriver.Firefox()
-firefox.get('https://thetypingcat.com/typing-speed-test/1m') #Change to 2m or 5m according to preference
 
 #Installing Extensions
+#Sometimes extensions disappear when navigating to a different URL, so we do it after we go to the website
+#This can cause some ads to still display
 for extension in extensions:
-    firefox.install_addon(extension, temporary=True)
+    firefox.install_addon(extensionPath + extension, temporary=True)
+#Going on 10FastFingers
+firefox.get('https://10fastfingers.com/typing-test/english')
 
-#Sleep so the website can load and not slow us down
+#Sleep so the website can load and not slow us down loading
 time.sleep(5) #Seconds
 
 # Using javascript to get the typing content from the website and storing value in "string" variable
-string = ''
-for i in range(firefox.execute_script('return document.querySelectorAll(".line").length')):
-	string += firefox.execute_script('return document.querySelectorAll(".line")['+str(i)+'].innerHTML')
-
-#Regex to delete tags present inside the string
-string = re.sub(r'<[^>]*>','',string) 
-
-#Regex to replace the '⏎' symbol with a carriage return so enter it presses it for us
-string = re.sub(r'\u23CE', '\\r',string) #Using \n will result in non-100% accuracy
-
-#Type the text!
-action = ActionChains(firefox)
-action.send_keys(" ") #Since we have to press a key to start the game first.
-action.send_keys(string)
-action.perform()
+#Type the word, then type space for every word.
+while firefox.execute_script('return document.querySelectorAll(".highlight").length') != 0:
+	string = firefox.execute_script('return document.querySelectorAll(".highlight")[0].innerHTML')
+	print(string)
+	
+    #Type the text!
+	action = ActionChains(firefox)
+	action.send_keys(string)
+	action.perform()
+	action = ActionChains(firefox)
+	action.send_keys(Keys.SPACE)
+	action.perform()
